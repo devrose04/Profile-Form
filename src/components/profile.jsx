890eead99f71f45b4e05d4d7ec89dac5f97cb91a
devrose04 <webdevrose04@gmail.com>
2023-08-03 21:28:09 +0900

    consruct

---
+++ src/components/profile.jsx
@@ -0,0 +1,12 @@
+import '../style/profile.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style/index.css';
+import App from './components/profile';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
